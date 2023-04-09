d89b7706296ca0212eb2a999893ca0c94f9afd07
Igor Oliveira <127331783+IgorOdSilva@users.noreply.github.com>
2023-04-09 13:19:10 -0300

    chat gpt incorporado ao site

---
+++ js/chat.js
@@ -0,0 +1,60 @@
+const inputQuestion = document.getElementById("inputQuestion");
+const result = document.getElementById("result");
+
+inputQuestion.addEventListener("keypress", (e) => {
+  if (inputQuestion.value && e.key === "Enter") SendQuestion();
+});
+
+
+
+const OPENAI_API_KEY = "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";
+
+result.disabled = true
+
+function SendQuestion() {
+  var sQuestion = inputQuestion.value;
+
+  fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + OPENAI_API_KEY,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: sQuestion,
+      max_tokens: 2048, // tamanho da resposta
+      temperature: 0.5, // criatividade na resposta
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (result.value) result.value += "\n";
+
+      if (json.error?.message) {
+        result.value += `Error: ${json.error.message}`;
+      } else if (json.choices?.[0].text) {
+        var text = json.choices[0].text || "Sem resposta";
+
+        result.value += "Informações encontradas: " + text;
+      }
+
+      result.scrollTop = result.scrollHeight;
+    })
+    .catch((error) => console.error("Error:", error))
+    .finally(() => {
+      inputQuestion.value = "";
+      inputQuestion.disabled = false;
+      inputQuestion.focus();
+    });
+
+  if (result.value) result.value += "\n\n\n";
+
+  result.value += `Sua pergunta: ${sQuestion}`;
+  inputQuestion.value = "Carregando...";
+  inputQuestion.disabled = true;
+  result.disabled = true
+
+  result.scrollTop = result.scrollHeight;
+}
