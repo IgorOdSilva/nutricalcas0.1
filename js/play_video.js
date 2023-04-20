2095301304763f28dfd78a59f18c53ff0103943b
Igor Oliveira <127331783+IgorOdSilva@users.noreply.github.com>
2023-04-20 10:24:58 -0300

    arrumei video

--- js/play_video.js
+++ js/play_video.js
@@ -1,7 +1,9 @@
 function playVideo() {
+   
     if (video1.style.display == 'block') {
         video1.style.display = 'none'
     } else {
         video1.style.display = 'block'
     }
 }
+
---
+++ js/play_video2.js
@@ -0,0 +1,7 @@
+function playVideo2() {
+    if (video2.style.display =='block') {
+        video2.style.display = 'none'
+    } else {
+        video2.style.display = 'block'
+    }
+}
