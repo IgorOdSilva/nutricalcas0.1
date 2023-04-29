58f249b016695e40e330e5e8b9fea8d45ad3beac
Igor Oliveira <127331783+IgorOdSilva@users.noreply.github.com>
2023-04-28 23:18:25 -0300

    peso ideal especifico generos

--- js/pi.js
+++ js/pi.js
@@ -1,9 +1,17 @@
-const resultadoPI = document.querySelector('#resultadoPI')
-const height = document.querySelector('#height')
-const weight = document.querySelector('#weight')
+function calcPI() {
 
-const calcPI = () => {
-const pi = (21 * (height.value * height.value)) .toFixed(1)
-resultadoPI.innerHTML = `Peso Ideal: ${pi}kg`
-
+    const resultadoPI = document.querySelector('#resultadoPI')
+    const height = document.querySelector('#height')
+    const weight = document.querySelector('#weight')
+    var fsex = document.getElementsByName('radsex')
+    var genero = ''
+    if (fsex[0].checked) {
+            genero = 'Homem'
+            var pi = (52 + (0.75 * ((height.value * 100) - 152.4))).toFixed(1)
+            resultadoPI.innerHTML = `Seu peso ideal é: ${pi}kg`
+    } else if (fsex[1].checked) {
+        genero = 'Mulher'
+        var pi = (52 + (0.67 * ((height.value * 100) - 152.4))).toFixed(1)
+        resultadoPI.innerHTML = `Seu peso ideal é: ${pi}kg`
+    }
 }
