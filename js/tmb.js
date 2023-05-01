026bc9be8c7b686163286267d423d53c3b532877
Igor Oliveira <127331783+IgorOdSilva@users.noreply.github.com>
2023-04-30 22:18:16 -0300

    NÃO SEI O Q TA ERRADOOOO

--- js/tmb.js
+++ js/tmb.js
@@ -1,11 +1,16 @@
 function calcTMB() {
-    const resultadoTMB = document.querySelector('#resultadoTMB')
-    const weight1 = document.querySelector('#weight1')
+    const resultadoTM = document.querySelector('#resultadoTM')
+    const pis = document.querySelector('#pis')
     const idade = document.querySelector('#idade')
     var sex = document.getElementsByName('sex')
     var gender = ''
     if (idade >= 0 && idade <=3 && sex[0].checked) {
         gender = 'Mem'
-        var tmb = ((59.512 * weight1.value) - 30.4).toFixed(1)
-        resultadoTMB.innerHTML = `${tmb} kcal`
+        var tmb = ((59.512 * pis.value) - 30.4).toFixed(1)
+        resultadoTMB.innerHTML = `Seu TMB é ${tmb} kcal`
+    }  else if (idade > 3 && idade <= 10 && sex[0].checked) {
+        gender = 'Mem'
+        var tmb = ((22.706 * pis.value) + 504.3)
+        resultadoTM.innerHTML = `${tmb}`
+    }
 }
